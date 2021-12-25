32839b8c4e8b95146bc24f48ad7a1ac947a1420b
Vikram Ramavarapu <vikramr2@illinois.edu>
2021-12-24 23:35:27 -0600

    Clouds

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 var h;
 var w;
 
+var clouds;
+var ys = [];
+var xs = [];
+
 var frame = 0;
 console.log("Change");
 
@@ -9,6 +13,21 @@ console.log("Change");
 function setup() {
 	h = window.innerHeight * 1.2;
 	w = window.innerWidth;
+
+	clouds = [
+		loadImage("assets/clouds/cloud0.png"),
+		loadImage("assets/clouds/cloud1.png"),
+		loadImage("assets/clouds/cloud2.png"),
+		loadImage("assets/clouds/cloud3.png"),
+		loadImage("assets/clouds/cloud4.png"),
+		loadImage("assets/clouds/cloud5.png")
+	];
+
+	for (var elem of clouds) { 
+		ys.push(Math.random()*(h/2-300)); 
+		xs.push(Math.random()*w*3);
+	}
+
   	canvas = createCanvas(w, h);
   	canvas.parent("canv")
 }
@@ -17,7 +36,7 @@ function setup() {
 function draw() {
 	console.log(w, h);
 	background(255, 255, 255);
-
+	
 	bottomColor = [255, 184, 219]
 	remColor = [181 - 255, 204 - 184, 255 - 219]
 
@@ -26,6 +45,14 @@ function draw() {
 		stroke(color(midColor[0], midColor[1], midColor[2]))
 		strokeWeight(1);
 		line(0, i, w, i)
+	}
+
+	for (var i = 0; i < clouds.length; i++) {
+		image(clouds[i], xs[i], ys[i]);
+		xs[i] += 2
+		if (xs[i] > 3*w) {
+			xs[i] = -w/4;
+		}
 	}
 
 	stroke(color(255, 255, 255));
